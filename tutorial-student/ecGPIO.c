580e938d24a46f171e056130bb4c8f93bb0a0628
ykkimhgu <38373000+ykkimhgu@users.noreply.github.com>
2021-09-28 17:21:53 +0900

    Add files via upload

---
+++ tutorial-student/ecGPIO.c
@@ -0,0 +1,27 @@
+// Distributed for LAB: GPIO
+
+#include "stm32f4xx.h"
+#include "stm32f411xe.h"
+#include "ecGPIO.h"
+
+void GPIO_init(GPIO_TypeDef *Port, int pin, int mode){     
+   
+	if (Port == GPIOA)
+		RCC_GPIOA_enable();
+	if (Port == GPIOC)
+		RCC_GPIOC_enable();
+	// Make it for GPIOB, GPIOD..GPIOH
+
+	// You can make a more general function of
+	// void RCC_GPIO_enable(GPIO_TypeDef *Port); 
+
+	GPIO_mode(Port, pin, mode);
+	// The rest are default values
+}
+
+
+// GPIO Mode          : Input(00), Output(01), AlterFunc(10), Analog(11, reset)
+void GPIO_mode(GPIO_TypeDef *Port, int pin, int mode){
+   Port->MODER &= ~(3UL<<(2*pin));     
+   Port->MODER |= mode<<(2*pin);    
+}
---
+++ tutorial-student/ecGPIO.h
@@ -0,0 +1,38 @@
+// Distributed for LAB: GPIO
+
+#include "stm32f411xe.h"
+#include "ecRCC.h"
+
+#ifndef __ECGPIO_H
+#define __ECGPIO_H
+
+#define INPUT  0x00
+#define OUTPUT 0x01
+#define AF     0x02
+#define ANALOG 0x03
+
+#define HIGH 1
+#define LOW  0
+
+#define LED_PIN 	5
+#define BUTTON_PIN 13
+
+#ifdef __cplusplus
+ extern "C" {
+#endif /* __cplusplus */
+	 
+void GPIO_init(GPIO_TypeDef *Port, int pin, int mode);
+void GPIO_write(GPIO_TypeDef *Port, int pin, int Output);
+int  GPIO_read(GPIO_TypeDef *Port, int pin);
+void GPIO_mode(GPIO_TypeDef* Port, int pin, int mode);
+void GPIO_ospeed(GPIO_TypeDef* Port, int pin, int speed);
+void GPIO_otype(GPIO_TypeDef* Port, int pin, int type);
+void GPIO_pudr(GPIO_TypeDef* Port, int pin, int pudr);
+
+
+ 
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif
