7670ec0c6855edc49bd0002da8785b7448cc4447
ykkimhgu <38373000+ykkimhgu@users.noreply.github.com>
2022-09-20 14:11:45 +0900

    Update ecGPIO_student.h

--- lab-tutorial/lib-student/ecGPIO_student.h
+++ lab-tutorial/lib-student/ecGPIO_student.h
@@ -1,4 +1,13 @@
-// Distributed for LAB: GPIO
+/*----------------------------------------------------------------\
+@ Embedded Controller by Young-Keun Kim - Handong Global University
+Author           : SSS LAB
+Created          : 05-03-2021
+Modified         : 09-20-2022
+Language/ver     : C++ in Keil uVision
+
+Description      : Distributed to Students for LAB_GPIO
+/----------------------------------------------------------------*/
+
 
 #include "stm32f411xe.h"
 #include "ecRCC.h"
