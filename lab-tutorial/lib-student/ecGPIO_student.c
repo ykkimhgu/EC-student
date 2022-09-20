08018bad3c55a4c8f34cde83c8e9fc6ed86afdaa
ykkimhgu <38373000+ykkimhgu@users.noreply.github.com>
2022-09-20 14:16:02 +0900

    Update ecGPIO_student.c

--- lab-tutorial/lib-student/ecGPIO_student.c
+++ lab-tutorial/lib-student/ecGPIO_student.c
@@ -15,23 +15,53 @@ Description      : Distributed to Students for LAB_GPIO
 #include "ecGPIO.h"
 
 void GPIO_init(GPIO_TypeDef *Port, int pin, int mode){     
-   
+	// mode  : Input(0), Output(1), AlterFunc(2), Analog(3)   
 	if (Port == GPIOA)
 		RCC_GPIOA_enable();
 	if (Port == GPIOC)
 		RCC_GPIOC_enable();
+	
+	//[TO-DO] YOUR CODE GOES HERE
 	// Make it for GPIOB, GPIOD..GPIOH
 
-	// You can make a more general function of
+	// You can also make a more general function of
 	// void RCC_GPIO_enable(GPIO_TypeDef *Port); 
 
 	GPIO_mode(Port, pin, mode);
-	// The rest are default values
+	
 }
 
 
-// GPIO Mode          : Input(00), Output(01), AlterFunc(10), Analog(11, reset)
+
+
+// GPIO Mode          : Input(00), Output(01), AlterFunc(10), Analog(11)
 void GPIO_mode(GPIO_TypeDef *Port, int pin, int mode){
    Port->MODER &= ~(3UL<<(2*pin));     
    Port->MODER |= mode<<(2*pin);    
 }
+
+
+// GPIO Speed          : Low speed (00), Medium speed (01), Fast speed (10), High speed (11)
+void GPIO_ospeed(GPIO_TypeDef *Port, int pin, int speed){
+	//[TO-DO] YOUR CODE GOES HERE
+	//[TO-DO] YOUR CODE GOES HERE
+}
+
+// GPIO Output Type: Output push-pull (0, reset), Output open drain (1)
+void GPIO_otype(GPIO_TypeDef *Port, int pin, int type){
+   	//[TO-DO] YOUR CODE GOES HERE
+	//[TO-DO] YOUR CODE GOES HERE
+}
+
+// GPIO Push-Pull    : No pull-up, pull-down (00), Pull-up (01), Pull-down (10), Reserved (11)
+void GPIO_pupdr(GPIO_TypeDef *Port, int pin, int pudr){
+   	//[TO-DO] YOUR CODE GOES HERE
+	//[TO-DO] YOUR CODE GOES HERE
+}
+
+int GPIO_read(GPIO_TypeDef *Port, int pin){
+   	//[TO-DO] YOUR CODE GOES HERE
+	//[TO-DO] YOUR CODE GOES HERE
+	return 0;    	//[TO-DO] YOUR CODE GOES HERE	
+}
+
