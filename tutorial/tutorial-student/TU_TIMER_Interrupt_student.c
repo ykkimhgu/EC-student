76eaeb9d76436159e66cb1d55074334ae2f209ab
hyKang <91526930+hyKangHGU@users.noreply.github.com>
2022-10-12 19:13:10 +0900

    update TU_Timer_Interrupt & PWM_student

--- tutorial/tutorial-student/TU_TIMER_Interrupt_student.c
+++ tutorial/tutorial-student/TU_TIMER_Interrupt_student.c
@@ -1,18 +1,17 @@
 /**
 ******************************************************************************
 * @author  SSSLAB
-* @Mod		 2021-8-12 by YKKIM  	
-* @brief   Embedded Controller:  Tutorial ___
-*					 - _________________________________
+* @Mod		 2022-8-12 by YKKIM  	
+* @brief   Embedded Controller:  Tutorial TIMER Interrupt
+*					 - LED Toggle every 1s
 * 
 ******************************************************************************
 */
 #include "stm32f411xe.h"
 #include "ecGPIO.h"
 #include "ecRCC.h"
-#include "ecTIM.h"
 
-uint32_t _count=0;
+uint32_t count=0;
 
 #define LED_PIN 	5
 
@@ -30,8 +29,9 @@ int main(void) {
 	timerx->DIER |=                     // Enable Interrupt
 	timerx->CR1 |=                      // Enable counter
 	
-	NVIC_EnableIRQ();				            // TIM2_IRQHandler Enable
-	NVIC_SetPriority( ,);               // TIM2_IRQHandler Set priority as 2
+	NVIC_SetPriority(, );               // TIM2_IRQHandler Set priority as 2
+	NVIC_EnableIRQ();				    // TIM2_IRQHandler Enable
+	
 	
 	// Inifinite Loop ----------------------------------------------------------
 	while(1){}
@@ -47,7 +47,7 @@ void setup(void)
 void TIM2_IRQHandler(void){
 	if((TIM2->SR & TIM_SR_UIF) == ){ // update interrupt flag
 		//Create the code to toggle LED by 1000ms
+
 		TIM2->SR &=                         // clear by writing 0
 	}
-	
 }
--- tutorial/tutorial-student/TU_TIMER_PWM_student.c
+++ tutorial/tutorial-student/TU_TIMER_PWM_student.c
@@ -10,9 +10,7 @@
 #include "stm32f411xe.h"
 #include "ecGPIO.h"
 #include "ecRCC.h"
-#include "ecPWM.h"
 #include "ecSysTick.h"
-#include "ecEXTI.h"
 
 #define LED_PIN 	5
 
@@ -22,46 +20,47 @@ int main(void) {
 	// Initialiization --------------------------------------------------------
 	RCC_PLL_init();                         // System Clock = 84MHz
 	SysTick_init();                         // for delay_ms()
-	GPIO_init(GPIOA, LED_PIN, EC_ALTE);     // GPIOA 5 ALTERNATE function
-	GPIO_ospeed(GPIOA, LED_PIN, EC_HIGH);   // GPIOA 5 HIGH SPEED
+	GPIO_init(GPIOA, LED_PIN, ______);     // GPIOA 5 ALTERNATE function
+	GPIO_ospeed(GPIOA, LED_PIN, ______);   // GPIOA 5 HIGH SPEED
 	
 	// TEMP: TIMER Register Initialiization --------------------------------------------------------		
 	TIM_TypeDef *TIMx;
 	TIMx = TIM2;
 	
 	// GPIO: ALTERNATIVE function setting
-	GPIOA->AFR[0]	 =                	                  // AF1 at PA5 = TIM2_CH1 (p.150)
+	GPIOA->AFR[0]	 =           						// AF1 at PA5 = TIM2_CH1 (p.150)
 	
 	// TIMER: PWM setting
-	RCC->APB1ENR |=                                     // Enable TIMER clock
+	RCC->APB1ENR |=               				// Enable TIMER clock
 	
-	TIMx->CR1 &= 				                                // Direction Up-count
+	TIMx->CR1 &= 				              		// Direction Up-count
 	
-	TIMx->PSC = 						                            // f_cnt = 10kHz
+	TIMx->PSC = 						              // Set Timer CLK = 100kHz : (PSC + 1) = 84MHz/100kHz --> PSC = ?
 	
-	TIMx->ARR = 									                      // Auto-reload: Upcounting (0...ARR).    
+	TIMx->ARR = 									        // Auto-reload: Upcounting (0...ARR). 
+																				// Set Counter Period = 1kHz : (ARR + 1) = 100kHz/1kHz --> ARR = ?
 	
-	TIMx->CCMR1 &= ~TIM_CCMR1_OC1M;                     // Clear ouput compare mode bits for channel 1
-	TIMx->CCMR1 |=                                      // OC1M = 110 for PWM Mode 1 output on ch1
-	TIMx->CCMR1	|= TIM_CCMR1_OC1PE;                     // Output 1 preload enable (make CCR1 value changable)
+	TIMx->CCMR1 &= ~TIM_CCMR1_OC1M;  			// Clear ouput compare mode bits for channel 1
+	TIMx->CCMR1 |=                   			// OC1M = 110 for PWM Mode 1 output on ch1
+	TIMx->CCMR1	|= TIM_CCMR1_OC1PE;    		// Output 1 preload enable (make CCR1 value changable)
 			
-	TIMx->CCER &= ~TIM_CCER_CC1P;                       // select output polarity: active high	
-	TIMx->CCER  |= 												              // Enable output for ch1
-	TIMx->CCR1  =       													      // Output Compare Register for channel 1 
+	TIMx->CCER &= ~TIM_CCER_CC1P;    			// select output polarity: active high	
+	TIMx->CCER |= 												// Enable output for ch1
+	TIMx->CCR1 =       										// Output Compare Register for channel 1 
 	
-	TIMx->CR1  |= TIM_CR1_CEN;                          // Enable counter
+	TIMx->CR1  |= TIM_CR1_CEN;      			// Enable counter
 
 	
 	// Inifinite Loop ----------------------------------------------------------
 	while(1){
-			//Create the code to change the brightness of LED as 10kHZ (use "delay(100)")
+			//Create the code to change the brightness of LED as 10kHZ (use "delay(1000)")
 		}
 	}
 // Initialiization 
 void setup(void)
 {	
-	RCC_PLL_init();                         // System Clock = 84MHz
-	SysTick_init();                         // for delay_ms()
+	RCC_PLL_init();       // System Clock = 84MHz
+	SysTick_init();       // for delay_ms()
 }
 
 
