28fe782a0cffcc175abc11c498d53714db5db40b
hyKang <91526930+hyKangHGU@users.noreply.github.com>
2022-11-28 09:52:21 +0900

    Update EC_zigbee_demo.c

--- tutorial/tutorial-student/EC_zigbee_demo.c
+++ tutorial/tutorial-student/EC_zigbee_demo.c
@@ -17,16 +17,18 @@
 // USART2 : MCU to PC via usb 
 // USART1 : MCU to MCU2 via zigbee
 
-uint8_t mcu2Data = 0;
-uint8_t pcData = 0;
-int indx =0;
-int maxBuf=10;
-uint8_t buffer[100]={0,};
-int bReceive=0;
-int ledOn = 0;
-int endChar = 13;
+#define END_CHAR 13
+#define MAX_BUF 10
 
-int i =0;
+// char data
+uint8_t recvChar = 0;
+uint8_t pcData = 0;
+
+uint8_t buffer[MAX_BUF] = {0,};
+int idx = 0;
+
+int bReceive = 0;
+int ledOn = 0;
 
 void setup(void);
 
@@ -38,18 +40,22 @@ int main(void) {
 	// Inifinite Loop ----------------------------------------------------------
 	while (1){
 		
-		if(bReceive==1 && buffer[0]=='L'){
-			printf("buffer : %s\r\n",buffer);
-			if (buffer[1]=='0') ledOn = 0;		
-			else if (buffer[1]=='1') ledOn = 1;
-			else printf("ERROR : Wrong command\r\n");
+		if(bReceive == 1 && buffer[0] == 'L'){
+			printf("buffer : %s\r\n", buffer);
+			
+			if 			(buffer[1] == '0')	ledOn = 0;		
+			else if (buffer[1] == '1') 	ledOn = 1;
+			else 												printf("ERROR : Wrong command\r\n");
+			
 			bReceive = 0;
+			memset(buffer, 0, sizeof(char) * MAX_BUF);
 		}
 
-		GPIO_write(GPIOA,5,ledOn);
+		GPIO_write(GPIOA, 5, ledOn);
 		delay_ms(1000);
 	}
 }
+
 
 // Initialiization 
 void setup(void)
@@ -59,29 +65,30 @@ void setup(void)
 	
 	// USART congfiguration
 	USART_init(USART2, 9600);
-	USART_begin(USART1, GPIOA,9,GPIOA,10, 9600); 	// PA9 - RXD , PA10 - TXD
+	USART_begin(USART6, GPIOA, 11, GPIOA, 12, 9600); 	// PA11: TXD, PA12: RXD
 	
 	// GPIO configuration
 	LED_init();
 }
 
 
-void USART1_IRQHandler(){		//USART1 INT 
-	if(is_USART_RXNE(USART1)){
-		mcu2Data = USART_getc(USART1);
-		//printf("%c",pcData); 						// echo to sender(pc)		
-		if(mcu2Data==endChar) {
-			bReceive=1;
-			indx = 0;
+void USART6_IRQHandler(){		//USART1 INT 
+	if(is_USART_RXNE(USART6)){
+		recvChar = USART_getc(USART6);
+		printf("%c", recvChar); 						// echo to sender(pc)		
+		
+		if(recvChar == END_CHAR) {
+			bReceive = 1;
+			idx = 0;
 		}
 		else{
-			if(indx>maxBuf){
-				indx =0;
-				memset(buffer, 0, sizeof(char) * maxBuf);
+			if(idx > MAX_BUF){
+				idx = 0;
+				memset(buffer, 0, sizeof(char) * MAX_BUF);
 				printf("ERROR : Too long string\r\n");
 			}
-			buffer[indx] = mcu2Data;
-			indx++;
+			buffer[idx] = recvChar;
+			idx++;
 		}
 	}
 }
@@ -89,10 +96,10 @@ void USART1_IRQHandler(){		//USART1 INT
 void USART2_IRQHandler(){		//USART2 INT 
 	if(is_USART_RXNE(USART2)){
 		pcData = USART_getc(USART2);
-		USART_write(USART1,&pcData,1);	// transmit char to USART1
-		printf("%c",pcData); 						// echo to sender(pc)
+		USART_write(USART6, &pcData, 1);	// transmit char to USART1
+		printf("%c",pcData); 							// echo to sender(pc)
 		
-		if(pcData==endChar){
+		if(pcData == END_CHAR){
 			printf("\r\n"); 							// to change line on PC display
 		}
 	}
