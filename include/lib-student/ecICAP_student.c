6b3e9c66e3dcf1593aa3e79bba19438ace28b809
ckdals915 <84508106+ckdals915@users.noreply.github.com>
2023-10-25 21:07:35 +0900

    ICAP Modified

--- include/lib-student/ecICAP_student.c
+++ include/lib-student/ecICAP_student.c
@@ -1,4 +1,4 @@
-#include "ecTIM.h"
+#include "ecICAP.h"
 #include "ecGPIO.h"
 #include "math.h"
 
@@ -17,13 +17,13 @@ void ICAP_init(PinName_t pinName){
 
 // GPIO configuration ---------------------------------------------------------------------	
 // 1. Initialize GPIO port and pin as AF
-	________________________;  							// GPIO init as AF=2
-	GPIO_ospeed(port, pin, ___);  						// speed VHIGH=3	
+	GPIO_init(port, pin, AF);  							// AF=2
+	GPIO_ospeed(port, pin, EC_HIGH);  						// speed VHIGH=3	
 
 // 2. Configure GPIO AFR by Pin num.
 	if(TIMx == TIM1 || TIMx == TIM2)											 port->AFR[pin >> 3] |= 0x01 << (4*(pin % 8)); // TIM1~2
-	else if  (____) ________________________;  // TIM3~5
-	else if  (____) ________________________;  // TIM9~11
+	else if(TIMx == TIM3 || TIMx == TIM4 || TIMx == TIM5)  port->AFR[pin >> 3] |= 0x02 << (4*(pin % 8)); // TIM3~5 
+	else if(TIMx == TIM9 || TIMx == TIM10|| TIMx == TIM11) port->AFR[pin >> 3] |= 0x03 << (4*(pin % 8)); // TIM9~11 
 
 	
 // TIMER configuration ---------------------------------------------------------------------			
@@ -43,9 +43,9 @@ void ICAP_init(PinName_t pinName){
 // 1. Select Timer channel(TIx) for Input Capture channel(ICx)
 	// Default Setting
 	TIMx->CCMR1 &= 	~TIM_CCMR1_CC1S;
-	TIMx->CCMR1 &=
-	TIMx->CCMR2 &=
-	TIMx->CCMR2 &=
+	TIMx->CCMR1 &= 	~TIM_CCMR1_CC2S;
+	TIMx->CCMR2 &= 	~TIM_CCMR2_CC3S;
+	TIMx->CCMR2 &= 	~TIM_CCMR2_CC4S;
 	TIMx->CCMR1 |= 	TIM_CCMR1_CC1S_0;      	//01<<0   CC1S    TI1=IC1
 	________________________;  				     	//01<<8   CC2s    TI2=IC2
 	________________________;        				//01<<0   CC3s    TI3=IC3
@@ -83,8 +83,8 @@ void ICAP_setup(PinName_t pinName, int ICn, int edge_type){
 	ICAP_pinmap(pinName, &TIMx, &CHn);
 
 // 1. Disable  CC. Disable CCInterrupt for ICn. 
-	TIMx->CCER ___________________;															// Capture Disable
-	TIMx->DIER ___________________;															// CCn Interrupt Disable	
+	TIMx->CCER &= ~(1 << (4*(ICn - 1))); 													// Capture Enable
+	TIMx->DIER &= ~(1 << ICn); 																// CCn Interrupt enabled	
 	
 	
 // 2. Configure  IC number(user selected) with given IC pin(TIMx_CHn)
@@ -92,12 +92,12 @@ void ICAP_setup(PinName_t pinName, int ICn, int edge_type){
 			case 1:
 					TIMx->CCMR1 &= ~TIM_CCMR1_CC1S;											//reset   CC1S
 					if (ICn==CHn) TIMx->CCMR1 |= 	TIM_CCMR1_CC1S_0;     //01<<0   CC1S    Tx_Ch1=IC1
-					else TIMx->CCMR1 |= TIM_CCMR1_CC1S_1;      											//10<<0   CC1S    Tx_Ch2=IC1
+					else TIMx->CCMR1 |= 	TIM_CCMR1_CC1S_1;      				//10<<0   CC1S    Tx_Ch2=IC1
 					break;
 			case 2:
-					TIMx->CCMR1 ___________________;										//reset   CC2S
-					if (ICn==CHn) TIMx->CCMR1 ___________________;     	//01<<0   CC2S    Tx_Ch2=IC2
-					else ___________________;     											//10<<0   CC2S    Tx_Ch1=IC2
+					TIMx->CCMR1 &= ~TIM_CCMR1_CC2S;											//reset   CC2S
+					if (ICn==CHn) TIMx->CCMR1 |= 	TIM_CCMR1_CC2S_0;     //01<<0   CC2S    Tx_Ch2=IC2
+					else TIMx->CCMR1 |= 	TIM_CCMR1_CC2S_1;      				//10<<0   CC2S    Tx_Ch1=IC2
 					break;
 			case 3:
 					TIMx->CCMR2 &= ~TIM_CCMR2_CC3S;											//reset   CC3S
@@ -114,9 +114,9 @@ void ICAP_setup(PinName_t pinName, int ICn, int edge_type){
 
 
 // 3. Configure Activation Edge direction
-	TIMx->CCER  ___________________;	  									// Clear CCnNP/CCnP bits for ICn
+	TIMx->CCER  &= ~(0b1010 << 4*(ICn - 1));  					// Clear CCnNP/CCnP bits
 	switch(edge_type){
-		case IC_RISE: TIMx->CCER ___________________;	 break; //rising:  00
+		case IC_RISE: TIMx->CCER &= ~(0b1010 << 4*(ICn - 1)); break; //rising:  00
 		case IC_FALL: TIMx->CCER ___________________;	 break; //falling: 01
 		case IC_BOTH: TIMx->CCER ___________________;	 break; //both:    11
 	}
@@ -146,9 +146,23 @@ uint32_t is_CCIF(TIM_TypeDef *TIMx, uint32_t ccNum){
 }
 
 void clear_CCIF(TIM_TypeDef *TIMx, uint32_t ccNum){
-	TIMx->SR ___________________;	
+	TIMx->SR &= ~(1 << ccNum);
 }
 
+uint32_t ICAP_capture(TIM_TypeDef* TIMx, uint32_t ICn){
+	uint32_t capture_Value;
+	
+	if (ICn == 1)
+		capture_Value = TIMx->CCR1;
+	else if (ICn == 2)
+		capture_Value = TIMx->CCR2;
+	else if (ICn == 2)
+		capture_Value = TIMx->CCR3;
+	else
+		capture_Value = TIMx->CCR4;
+
+	return capture_Value;
+}
 
 //DO NOT MODIFY THIS
 void ICAP_pinmap(PinName_t pinName, TIM_TypeDef **TIMx, int *chN){
--- include/lib-student/ecICAP_student.h
+++ include/lib-student/ecICAP_student.h
@@ -1,5 +1,5 @@
-#ifndef __EC_TIM_H
-#define __EC_TIM_H
+#ifndef __EC_ICAP_H
+#define __EC_ICAP_H
 #include "stm32f411xe.h"
 
 #ifdef __cplusplus
@@ -18,6 +18,12 @@
 #define IC_FALL 1
 #define IC_BOTH 2
 
+// Input Capture Number
+#define IC_1    1
+#define IC_2    2
+#define IC_3    3
+#define IC_4    4
+
 //Input Capture
 
 void ICAP_pinmap(PinName_t pinName, TIM_TypeDef **TIMx, int *chN);
@@ -25,6 +31,8 @@ void ICAP_pinmap(PinName_t pinName, TIM_TypeDef **TIMx, int *chN);
 void ICAP_init(PinName_t pinName);
 void ICAP_setup(PinName_t pinName, int ICn, int edge_type);
 void ICAP_counter_us(PinName_t pinName, int usec);
+uint32_t ICAP_capture(TIM_TypeDef* TIMx, uint32_t ICn);
+
 uint32_t is_CCIF(TIM_TypeDef *TIMx, uint32_t CCnum);  // CCnum= 1~4
 void clear_CCIF(TIM_TypeDef *TIMx, uint32_t CCnum);
 
