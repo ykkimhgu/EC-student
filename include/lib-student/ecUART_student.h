8e4ede5ede5dd7417bcf1683f7f9978d22ca8577
ykkimhgu <38373000+ykkimhgu@users.noreply.github.com>
2023-11-06 21:29:58 +0900

    Update ecUART_student.h

--- include/lib-student/ecUART_student.h
+++ include/lib-student/ecUART_student.h
@@ -10,46 +10,46 @@
 #define INT 1
 
 // You can modify this
-#define BAUD_9600		9600
+#define BAUD_9600	9600
 #define BAUD_19200	19200
-#define BAUD_38400  38400
+#define BAUD_38400  	38400
 #define BAUD_57600	57600
-#define BAUD_115200 115200
-#define BAUD_921600 921600
+#define BAUD_115200 	115200
+#define BAUD_921600 	921600
 
 
 // ********************** USART 2 (USB) ***************************
-	// PA_2 = USART2_TX
-	// PA_3 = USART2_RX
-	// Alternate function(AF7), High Speed, Push pull, Pull up
-	// APB1
+// PA_2 = USART2_TX
+// PA_3 = USART2_RX
+// Alternate function(AF7), High Speed, Push pull, Pull up
+// APB1
 // **********************************************************
 
-	// ********************** USART 1 ***************************
-	// PA_9 = USART1_TX (default)	// PB_6  (option)
-	// PA_10 = USART1_RX (default)	// PB_3 (option)
-	// APB2
-	// **********************************************************
+// ********************** USART 1 ***************************
+// PA_9 = USART1_TX (default)	// PB_6  (option)
+// PA_10 = USART1_RX (default)	// PB_3 (option)
+// APB2
+// **********************************************************
 
 // ********************** USART 6 ***************************
-	// PA_11 = USART6_TX (default)	// PC_6  (option)
-	// PA_12 = USART6_RX (default)	// PC_7 (option)
-	// APB2
+// PA_11 = USART6_TX (default)	// PC_6  (option)
+// PA_12 = USART6_RX (default)	// PC_7 (option)
+// APB2
 // **********************************************************
 
-// Configuration UART 1, 2 using default pins
+// Configuration UART 1, 2 using default pins 
 void UART1_init(void);
 void UART2_init(void);	
 void UART1_baud(uint32_t baud);
 void UART2_baud(uint32_t baud);
 
-// Configuration USART write & read
+// USART write & read
 void USART1_write(uint8_t* buffer, uint32_t nBytes);
 void USART2_write(uint8_t* buffer, uint32_t nBytes);
 uint8_t USART1_read(void);										
 uint8_t USART2_read(void);	
 
-// Inturrupt USART1,2
+// RX Inturrupt Flag USART1,2
 uint32_t is_USART1_RXNE(void);
 uint32_t is_USART2_RXNE(void);
 
