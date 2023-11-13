4a35ac20216439f998d78d608ff74c9ed57800e8
ckdals915 <84508106+ckdals915@users.noreply.github.com>
2023-11-13 19:19:34 +0900

    ADC Updated

--- include/lib-student/ecADC_student.c
+++ include/lib-student/ecADC_student.c
@@ -1,208 +1,437 @@
 #include "stm32f411xe.h"
 #include "ecSysTick.h"
-#include "ecADC_student.h"
+#include "ecADC.h"
 #include "ecGPIO.h"
 #include "ecTIM.h"
 #include <stdint.h>
-uint32_t result;
 
-void ADC_init(GPIO_TypeDef *port, int pin, int trigmode){  //mode 0 : SW, 1 : TRGO
+/* -------------------------------------------------------------------------------------*/
+//									ADC Configuration																										//
+/* -------------------------------------------------------------------------------------*/
+
+void ADC_init(PinName_t pinName){  // trigmode 0 : SW, 1 : TRGO
+
 // 0. Match Port and Pin for ADC channel	
-	int CHn = ADC_pinmap(port, pin);			// ADC Channel <->Port/Pin mapping
+	GPIO_TypeDef *port;
+	unsigned int pin;	
+	ecPinmap(pinName, &port, &pin);
+	int chN;
+	ADC_pinmap(pinName, &chN);			// ADC Channel <->Port/Pin mapping
 
 // GPIO configuration ---------------------------------------------------------------------	
 // 1. Initialize GPIO port and pin as ANALOG, no pull up / pull down
 	GPIO_init(port, pin, ANALOG);  				// ANALOG = 3
-	GPIO_pupdr(port, pin, EC_NONE);  			// EC_NONE = 0
+	GPIO_pupd(port, pin, EC_NONE);  			// EC_NONE = 0
 
 // ADC configuration	---------------------------------------------------------------------			
 // 1. Total time of conversion setting
 	// Enable ADC pheripheral clock
-	RCC->APB2ENR |= ___________; 		// Enable the clock of RCC_APB2ENR_ADC1EN
+	RCC->APB2ENR  |= RCC_APB2ENR_ADC1EN; 		// Enable the clock of RCC_APB2ENR_ADC1EN
 	
 	// Configure ADC clock pre-scaler
-	ADC->CCR &= ___________;					// 0000: PCLK2 divided by 2	(42MHz)
+	ADC->CCR &= ~ADC_CCR_ADCPRE;				// 0000: PCLK2 divided by 2	(42MHz)
 	
 	// Configure ADC resolution 
-	ADC1->CR1 &= ___________;     		// 00: 12-bit resolution (15cycle+)
+	ADC1->CR1 &= ~ADC_CR1_RES;     				// 00: 12-bit resolution (15cycle+)
 	
 	// Configure channel sampling time of conversion.	
 	// Software is allowed to write these bits only when ADSTART=0 and JADSTART=0	!!
 	// ADC clock cycles @42MHz = 2us
-	if(CHn < 10) {
-		ADC1->SMPR2 &= ___________;								// clear bits
-		ADC1->SMPR2 |= 4U << ___________;					// sampling time conversion : 84  			
+	if(chN < 10) {
+		ADC1->SMPR2 &= ~(7UL << (3* chN));			// clear bits
+		ADC1->SMPR2 |= 4U << (3* chN);				// sampling time conversion : 84  			
 	}
 	else{
-		ADC1->SMPR1 &= ___________;
-		ADC1->SMPR1 |= 4U << ___________;
+		ADC1->SMPR1 &= ~(7UL << (3* (chN - 10)));
+		ADC1->SMPR1 |= 4U << (3* (chN - 10));
 	}
 	
 // 2. Regular / Injection Group 
 	//Regular: SQRx, Injection: JSQx
 
-// 3. Repetition: Single or Continuous conversion
-	ADC1->CR2 &= ___________;      			// default : Single conversion mode
+// 3. Repetition: Single scan or Continuous scan conversion
+	ADC1->CR2 |= ___________;      			// default : Continuous conversion mode	
 	
-// 4. Single Channel or Scan mode
-	// Configure the sequence length
-	ADC1->SQR1 &= ___________; 					// 0000: 1 conversion in the regular channel conversion sequence
+// 4. Single(one) Channel or Scan(multi-channel) mode  
+	// Configure the sequence length			// default: one-channel length
+	ADC1->SQR1 &= ___________; 					// 0000: one channel length in the regular channel conversion sequence
 	
-	// Configure the channel sequence 
-	ADC1->SQR3 &= ___________;				 	// SQ1 clear bits
-	ADC1->SQR3 |= (CHn & ___________); 	// Choose the channel to convert firstly
-		
-	// Single Channel: scan mode, right alignment
-	ADC1->CR1 |= ___________;						// 1: Scan mode enable 
-	ADC1->CR2 &= ___________;   				// 0: Right alignment	
+	// Configure the multiple channel sampling sequence 
+	ADC1->SQR3 &= ~ADC_SQR3_SQ1;				// SQ1 clear 
+	ADC1->SQR3 |= ___________; 		// Choose the first channelID to sample
+	
+	// Default:  Single(one-channel) Channel mode 
+	ADC1->CR1 &= ___________;					// 0: One-channel mode 		
 	
 // 5. Interrupt Enable
 	// Enable EOC(conversion) interrupt. 
-	ADC1->CR1 &= ___________;          	// Interrupt reset
+	ADC1->CR1 &= ~ADC_CR1_EOCIE;          // Interrupt reset
 	ADC1->CR1 |= ___________;           // Interrupt enable
 	
 	// Enable ADC_IRQn 
-	NVIC_SetPriority(___________); 			// Set Priority to 2
-	NVIC_EnableIRQ(___________);      	// Enable interrupt form ACD1 peripheral	
+	NVIC_SetPriority(ADC_IRQn, 2); 				// Set Priority to 2
+	NVIC_EnableIRQ(ADC_IRQn);      				// Enable interrupt form ACD1 peripheral	
 
+
+	// Hardware Trigger Configuration : TIM3, 1msec, RISE edge
+	ADC_trigger(TIM3, 1, RISE);
+
+	// Start ADC 
+	ADC_start();
+}
+
+
+void ADC_trigger(TIM_TypeDef* TIMx, int msec, int edge){
+	// set timer
+	int timer = 0;
+	if(TIMx == TIM2) timer = 2;
+	else if(TIMx == TIM3) timer = 3;	
+	
+	// Single conversion mode (disable continuous conversion)
+	ADC1->CR2 &= ~ADC_CR2_CONT;     // Discontinuous conversion mode
+	ADC1->CR2 |= ADC_CR2_EOCS;  		// Enable EOCS
+	
+	// Enable TIMx Clock as TRGO mode	
+// 1. TIMx Trigger Output Config
+	// Enable TIMx Clock
+	TIM_init(TIMx, msec);
+	TIMx->CR1 &= ~TIM_CR1_CEN; 								//counter disable
+	
+	// Set PSC, ARR
+  TIM_period_ms(TIMx, msec);
+	
+  // Master Mode Selection MMS[2:0]: Trigger output (TRGO)
+  TIMx->CR2 &= ~TIM_CR2_MMS; 				// reset MMS
+  TIMx->CR2 |= TIM_CR2_MMS_2 | TIM_CR2_MMS_1;   		//100: Compare - OC1REF signal is used as trigger output (TRGO)
+   
+	// Output Compare Mode
+  TIMx->CCMR2 &= ~TIM_CCMR2_OC3M;       							// OC1M : output compare 1 Mode 
+  TIMx->CCMR2 |= TIM_CCMR2_OC3M_1 | TIM_CCMR2_OC3M_2; // OC1M = 110 for compare 1 Mode ch1 
+
+  // OC1 signal 
+  TIMx->CCER |= TIM_CCER_CC3E;  // CC1E Capture enabled
+	TIMx->CCR3  = (TIMx->ARR)/2;  // duty ratio 50%
+   
+  // Enable TIMx 
+  TIMx->CR1 |= TIM_CR1_CEN; 							//counter enable
+	
+// 2. ADC HW Trigger Config.
+	// Select Trigger Source  			
+	ADC1->CR2 &= ~ADC_CR2_EXTSEL; 			// reset EXTSEL
+	ADC1->CR2 |= (timer*2 + 2) << 24; 	// TIMx TRGO event (ADC : TIM2, TIM3 TRGO)
+	
+	//Select Trigger Polarity
+	ADC1->CR2 &= ~ADC_CR2_EXTEN;		// reset EXTEN, default
+	if(edge == RISE) ADC1->CR2 |= ADC_CR2_EXTEN_0;				// trigger detection rising edge
+	else if(edge == FALL) ADC1->CR2 |= ADC_CR2_EXTEN_1;		// trigger detection falling edge
+	else if(edge == BOTH) ADC1->CR2 |= ADC_CR2_EXTEN_Msk;	// trigger detection both edge
+}
+
+void ADC_conversion(int convMode){
+	if(convMode == CONT){
+		// Repetition: Continuous conversion
+		ADC1->CR2 |= ADC_CR2_CONT;      		// Enable Continuous conversion mode	
+	}
+	else if(convMode == SINGLE){
+		// Repetition: Single conversion
+		ADC1->CR2 &= ~ADC_CR2_CONT;      		// Disable Continuous conversion mode	
+	}
+} 
+
+void ADC_sequence(PinName_t *seqCHn, int seqCHnums){
+
+	// Disable ADC 
+	ADC1->CR2 &= ~ADC_CR2_ADON;
+	
+	// Initialize ADC channels
+	int chN[seqCHnums];
+
+	// Change to Multi-Channel mode(scan mode) 
+	if (seqCHnums > 1)
+		ADC1->CR1 |= ADC_CR1_SCAN;					// 1: (multi-channel)Scan mode  
+	
+	// ADC channels mapping
+	for(int k = 0; k < seqCHnums; k++)
+		ADC_pinmap(seqCHn[k], &(chN[k]));
+
+	ADC1->SQR1 &= ~ ADC_SQR1_L; 					// reset length of conversions in the regular channel 	
+	ADC1->SQR1 |= (seqCHnums - 1) << ADC_SQR1_L_Pos; // conversions in the regular channel conversion sequence
+	
+	for(int i = 0; i < seqCHnums; i++){
+		if (i < 6){
+			ADC1->SQR3 &= ~(0x1F << i*5);				// SQn clear bits
+			ADC1->SQR3 |= chN[i] << i*5;				// Choose the channel to convert sequence
+		}
+		else if (i <12){
+			ADC1->SQR2 &= ~(0x1F << (i-6)*5);		// SQn clear bits
+			ADC1->SQR2 |= chN[i] << (i-6)*5;		// Choose the channel to convert sequence
+		}
+		else{
+			ADC1->SQR1 &= ~(0x1F << (i-12)*5);	// SQn clear bits
+			ADC1->SQR1 |= chN[i] << (i-12)*5;		// Choose the channel to convert sequence
+		}
+	}
+
+	// Start ADC 
+	ADC_start();	
+}
+
+
+void ADC_start(void){
+	// Enable ADON, SW Trigger-------------------------------------------------------------------------------
+	ADC1->CR2 |= ADC_CR2_ADON;
+	ADC1->CR2 |= ADC_CR2_SWSTART;
+}
+
+// ADC value read
+uint32_t ADC_read(void){
+	return ADC1->DR;
+}
+
+// ADC interrupt flag
+uint32_t is_ADC_EOC(void){
+	return (ADC1->SR & ADC_SR_EOC) == ADC_SR_EOC;
+}
+
+// ADC overflow flag
+uint32_t is_ADC_OVR(void){
+	return (ADC1->SR & ADC_SR_OVR) == ADC_SR_OVR;
+}
+
+// ADC clear flag
+void clear_ADC_OVR(void){
+	ADC1->SR &= ~ADC_SR_OVR;
+}
+
+
+/* -------------------------------------------------------------------------------------*/
+//									JADC Configuration																									//
+/* -------------------------------------------------------------------------------------*/
+
+void JADC_init(PinName_t pinName){
+// 0. Match Port and Pin for JADC channel	
+	GPIO_TypeDef *port;
+	unsigned int pin;	
+	ecPinmap(pinName, &port, &pin);
+	int chN;
+	ADC_pinmap(pinName, &chN);			// ADC Channel <->Port/Pin mapping
+
+// GPIO configuration ---------------------------------------------------------------------	
+// 1. Initialize GPIO port and pin as ANALOG, no pull up / pull down
+	GPIO_init(port, pin, ANALOG);  				// ANALOG = 3
+	GPIO_pupd(port, pin, EC_NONE);  			// EC_NONE = 0
+
+// ADC configuration	---------------------------------------------------------------------			
+// 1. Total time of conversion setting
+	// Enable ADC pheripheral clock	
+	RCC->APB2ENR  |= RCC_APB2ENR_ADC1EN; 	// Enable the clock of RCC_APB2ENR_ADC1EN
+	
+	// Configure ADC clock pre-scaler
+	ADC->CCR &= ~ADC_CCR_ADCPRE;					// 0000: PCLK2 divided by 2	(42MHz)
+	
+	// Configure ADC resolution 
+	ADC1->CR1 &= ~ADC_CR1_RES;     				// 00: 12-bit resolution (15cycle+)
+	
+	// Configure channel sampling time of conversion.	
+	// Software is allowed to write these bits only when ADSTART=0 and JADSTART=0	!!
+	// ADC clock cycles @42MHz = 2us
+	if(chN < 10) {
+		ADC1->SMPR2  &= ~(7 << (3* chN));  			
+		ADC1->SMPR2  |= 4U << (3* chN);  			
+	}
+	else{
+		ADC1->SMPR1  &= ~(7 << (3* (chN - 10)));
+		ADC1->SMPR1  |= 4U << (3* (chN - 10));
+	}
+// 2. Regular / Injection Group 
+	//Regular: SQRx, Injection: JSQx
+
+// 3. Repetition: Single or Continuous conversion
+	ADC1->CR2 |= ADC_CR2_CONT;     				// Enable Continuous conversion mode	
+	
+// 4. Single Channel or Scan mode
+	//  - Single Channel: scan mode, right alignment	
+	ADC1->CR1 |= ADC_CR1_SCAN;						// 1: Scan mode enable 
+	ADC1->CR2 &= ~ADC_CR2_ALIGN;   				// 0: Right alignment	
+
+	// Configure the sequence length
+	ADC1->JSQR &= ~ADC_JSQR_JL; 					// 0000: 1 conversion in the regular channel conversion sequence
+
+	// Configure the channel sequence 
+	ADC1->JSQR &= ~ADC_JSQR_JSQ4;				 	// SQ1 clear bits
+	ADC1->JSQR |= (chN & ADC_JSQR_JSQ4); 	// Choose the channel to convert firstly
+
+// 5. Interrupt Enable
+	// Enable JEOC(conversion) interrupt.
+	ADC1->CR1 &= ~ADC_CR1_JEOCIE;   			// JEOC interrupt reset
+	ADC1->CR1 |= ADC_CR1_JEOCIE;					// JEOC interrupt enable
+	 
+	// Enable ADC_IRQn 
+	NVIC_SetPriority(ADC_IRQn,1); 				//NVIC interrupt setting
+	NVIC_EnableIRQ(ADC_IRQn);     				//Enable NVIC
 
 /* -------------------------------------------------------------------------------------*/
 //					HW TRIGGER MODE
 /* -------------------------------------------------------------------------------------*/
 	
 	// TRGO Initialize : TIM3, 1msec, RISE edge
-	if(trigmode == TRGO) ADC_TRGO(TIM3, 1, RISE);				
-	
+	JADC_trigger(TIM5, 1, RISE);
 }
 
-void ADC_TRGO(TIM_TypeDef* TIMx, int msec, int edge){
+void JADC_trigger(TIM_TypeDef* TIMx, int msec, int edge){
 	// set timer
 	int timer = 0;
-	if(TIMx == TIM2) timer=2;
-	else if(TIMx == TIM3) timer=3;	
+	if(TIMx==TIM1) timer=1;
+	else if(TIMx==TIM2) timer=2;
+	else if(TIMx==TIM4) timer=4;
+	else if(TIMx==TIM5) timer=5;
 	
 	// Single conversion mode (disable continuous conversion)
-	ADC1->CR2 &= ___________;     			// Discontinuous conversion mode
-	ADC1->CR2 |= ___________;  					// Enable EOCS
+	ADC1->CR2 &= ~ADC_CR2_CONT;     // Discontinuous conversion mode
+	ADC1->CR2 |= ADC_CR2_EOCS;  		// Enable EOCS
 	
-
-// HW Trigger configuration -------------------------------------------------------------
-	
+	// Enable TIMx Clock as TRGO mode
 // 1. TIMx Trigger Output Config
 	// Enable TIMx Clock
 	TIM_init(TIMx, msec);
-	TIMx->CR1 &= ___________; 					//counter disable
+	TIMx->CR1 &= ~1; 							//counter disable
 	
 	// Set PSC, ARR
- // TIM_period_ms(TIMx, msec);
+  TIM_period_ms(TIMx, msec);
 	
   // Master Mode Selection MMS[2:0]: Trigger output (TRGO)
-  TIMx->CR2 &= ___________; 					// reset MMS
-  TIMx->CR2 |= ___________;   				//100: Compare - OC1REF signal is used as trigger output (TRGO)
+  TIMx->CR2 &=~(7<<4); 					// reset MMS
+  TIMx->CR2 |= 4<<4;   					//100: Compare - OC1REF signal is used as trigger output (TRGO)
    
 	// Output Compare Mode
-  TIMx->CCMR1 &= ~TIM_CCMR1_OC1M;       			// OC1M : output compare 1 Mode 
-  TIMx->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2;  // OC1M = 110 for compare 1 Mode ch1 
+  TIMx->CCMR1 &= ~(7<<4);       // OC1M : output compare 1 Mode 
+  TIMx->CCMR1 |= 6<<4;          // OC1M = 110 for compare 1 Mode ch1 
 	
   // OC1 signal 
-  TIMx->CCER |= ___________;          // CC1E Capture enabled
-	TIMx->CCR1  = (TIMx->ARR)/2; 				// duty ratio 50%
-      
+  TIMx->CCER |=1;            		// CC1E Capture enabled
+	TIMx->CCR1  = (TIMx->ARR)/2;  //msec*10 - 1; 		// CCR set
+   
   // Enable TIMx 
-  TIMx->CR1 |= ___________; 			//counter enable
-
+  TIMx->CR1 |= 1; 							//counter enable
+	
 // 2. ADC HW Trigger Config.
 	// Select Trigger Source  			
-	ADC1->CR2 &= ~ADC_CR2_EXTSEL; 			// reset EXTSEL
-	ADC1->CR2 |= (timer*2 + 2) << 24; 			// TIMx TRGO event (ADC : TIM2, TIM3 TRGO)
+	ADC1->CR2 &= ~ADC_CR2_JEXTSEL; 	// reset EXTSEL
+	if(TIMx==TIM1) ADC1->CR2 |= timer<<16; 								// TIMx TRGO event (JADC : TIM1, TIM2, TIM4, TIM5 TRGO)
+	else if(TIMx==TIM2) ADC1->CR2 |= (timer+1)<<16; 			// TIMx TRGO event (JADC : TIM1, TIM2, TIM4, TIM5 TRGO)
+	else ADC1->CR2 |= (timer*2+1)<<16; 										// TIMx TRGO event (JADC : TIM1, TIM2, TIM4, TIM5 TRGO)
+			
+	ADC1->CR2 &= ~ADC_CR2_JEXTEN;													// reset JEXTEN, default
+	if(edge==RISE) ADC1->CR2 |= ADC_CR2_JEXTEN_0;					// trigger detection rising edge
+	else if(edge==FALL) ADC1->CR2 |= ADC_CR2_JEXTEN_1;		// trigger detection falling edge
+	else if(edge==BOTH) ADC1->CR2 |= ADC_CR2_JEXTEN_Msk;	// trigger detection both edge
+
+}
+
+void JADC_sequence(PinName_t *seqCHn, int seqCHnums){
+	// Disable ADC 
+	ADC1->CR2 &= ~ADC_CR2_ADON;
+
+	int chN[seqCHnums];
+
+	// Chnage to Multi-Channel mode(scan mode) 
+	if (seqCHnums>1)
+		ADC1->CR1 |= ADC_CR1_SCAN;					// 1: (multi-channel)Scan mode 
+
+	for(int k=0; k<seqCHnums; k++)					// ADC Channel <->Port/Pin mapping
+		ADC_pinmap(seqCHn[k], &(chN[k]));
+
+	ADC1->JSQR &= ~(0xF<<20); 					// reset length of conversions in the regular channel 	
+	ADC1->JSQR |= (seqCHnums-1)<<20; 			// conversions in the regular channel conversion sequence
 	
-	//Select Trigger Polarity
-	ADC1->CR2 &= ~ADC_CR2_EXTEN;				// reset EXTEN, default
-	if(edge==RISE) ADC1->CR2 |= ADC_CR2_EXTEN_0;				// trigger detection rising edge
-	else if(edge==FALL) ADC1->CR2 |= ADC_CR2_EXTEN_1;		// trigger detection falling edge
-	else if(edge==BOTH) ADC1->CR2 |= ADC_CR2_EXTEN_Msk;	// trigger detection both edge
+	for(int i = 0; i<seqCHnums; i++){
+		ADC1->JSQR &= ~(0x1F<<(5*i+15-(seqCHnums-1)*5));	// SQ1 clear bits
+		ADC1->JSQR |= chN[i]<<(5*i+15-(seqCHnums-1)*5);	// Choose the channel to convert sequence
+	}
 
+	// Start ADC 
+	JADC_start();	
 }
 
-void ADC_continue(int contmode){
-	if(contmode == CONT){
-		// Repetition: Continuous conversion
-		ADC1->CR2 |= ___________;      			// Enable Continuous conversion mode	
-		ADC1->CR1 &= ~ADC_CR1_SCAN;					// 0: Scan mode disable 
-	}
-	else { 																//if(contmode==SINGLE)
-		// Repetition: Single conversion
-		ADC1->CR2 &= ~ADC_CR2_CONT;      		// Disable Continuous conversion mode	
-		ADC1->CR1 |= ADC_CR1_SCAN;					// 1: Scan mode enable
-	}
-} 
 
-void ADC_sequence(int length, int *seq){
+// void JADC_sequence(int length, int *seq){
 	
-	ADC1->SQR1 &= ~ADC_SQR1_L; 										// reset length of conversions in the regular channel 	
-	ADC1->SQR1 |= (length - 1) << ADC_SQR1_L_Pos; // conversions in the regular channel conversion sequence
+// 	ADC1->JSQR &= ~(0xF<<20); 					// reset length of conversions in the regular channel 	
+// 	ADC1->JSQR |= (length-1)<<20; 			// conversions in the regular channel conversion sequence
 	
-	for(int i = 0; i<length; i++){
-		if (i<6){
-			ADC1->SQR3 &= ~(0x1F << i*5);				// SQn clear bits
-			ADC1->SQR3 |= seq[i] << i*5;				// Choose the channel to convert sequence
-		}
-		else if (i < 12){
-			ADC1->SQR2 &= ___________;				// SQn clear bits
-			ADC1->SQR2 |= ___________;				// Choose the channel to convert sequence
-		}
-		else{
-			ADC1->SQR1 &= ~(0x1F << (i-12)*5);	// SQn clear bits
-			ADC1->SQR1 |= seq[i] << (i-12)*5;		// Choose the channel to convert sequence
+// 	for(int i = 0; i<length; i++){
+// 		ADC1->JSQR &= ~(0x1F<<(5*i+15-(length-1)*5));	// SQ1 clear bits
+// 		ADC1->JSQR |= seq[i]<<(5*i+15-(length-1)*5);	// Choose the channel to convert sequence
+// 	}
+// }
+
+void JADC_start(void){
+	// Enable ADON, JSW Trigger-------------------------------------------------------------------------------
+	ADC1->CR2 |= ADC_CR2_ADON;
+	ADC1->CR2 |= ADC_CR2_JSWSTART;
+}
+
+uint32_t JADC_read(int chN){
+	uint32_t outData=0;
+
+	switch(chN){
+		case 1: outData = ADC1->JDR1;
+		case 2: outData = ADC1->JDR2;
+		case 3: outData = ADC1->JDR3;
+		case 4: outData = ADC1->JDR4;
+	}
+
+	return outData;
+}
+
+
+uint32_t is_JADC_EOC(void){
+	return (ADC1->SR & ADC_SR_JEOC) == ADC_SR_JEOC;
+}
+
+uint32_t is_JADC_OVR(void){
+	return (ADC1->SR & ADC_SR_OVR) == ADC_SR_OVR;
+}
+
+void clear_JADC_OVR(void){
+	ADC1->SR &= ~ADC_SR_OVR;
+}
+
+
+///////////////////////////////////////////////////
+void ADC_pinmap(PinName_t pinName, uint32_t *chN){
+	GPIO_TypeDef *port;
+	unsigned int pin;		
+	ecPinmap(pinName, &port, &pin);
+
+	if(port == GPIOA){
+		switch(pin){
+			case 0 : *chN = 0; break;
+        	case 1 : *chN = 1; break;
+        	case 4 : *chN = 4; break;
+        	case 5 : *chN = 5; break;
+        	case 6 : *chN = 6; break;
+        	case 7 : *chN = 7; break;
+        	default: break;
 		}
 	}
-}
-
-void ADC_start(void){
-	// Enable ADON, SW Trigger-------------------------------------------------------------------------------
-	ADC1->CR2 |= ___________;
-	ADC1->CR2 |= ___________;
-}
-
-uint32_t is_ADC_EOC(void){
-	return (ADC1->SR & ___________) == __________;
-}
-
-uint32_t is_ADC_OVR(void){
-	return (ADC1->SR & ___________) == __________;
-}
-
-void clear_ADC_OVR(void){
-	ADC1->SR &= ___________;
-}
-
-uint32_t ADC_read(){
-	return ADC1->___________;
-}
-
-uint32_t ADC_pinmap(GPIO_TypeDef *Port, int Pin){
-	if(Port == GPIOA){
-		if(Pin == 0) 			return 0;
-		else if(Pin == 1) return 1;
-		else if(Pin == 4) return 4;
-		else if(Pin == 5) return 5;
-		else if(Pin == 6) return 6;
-		else if(Pin == 7) return 7;
-		else 							while(1);
+	else if(port == GPIOB){
+		switch(pin){
+			case 0 : *chN = 8; break;
+        	case 1 : *chN = 9; break;
+        	default: break;
+		}
 	}
-	else if(Port == GPIOB){
-		if(Pin == 0) 			return 8;
-		else if(Pin == 1)	return 9;
-		else 							while(1);
-	}
-	else if(Port == GPIOC){
-		if(Pin == 0)			return 10;
-		else if(Pin == 1)	return 11;
-		else if(Pin == 2)	return 12;
-		else if(Pin == 3)	return 13;
-		else if(Pin == 4)	return 14;
-		else if(Pin == 5)	return 15;
-		else							while(1);
+	else if(port == GPIOC){
+		switch(pin){
+			case 0 : *chN = 10; break;
+        	case 1 : *chN = 11; break;
+			case 2 : *chN = 12; break;
+        	case 3 : *chN = 13; break;
+			case 4 : *chN = 14; break;
+        	case 5 : *chN = 15; break;
+        	default: break;
+		}
 	}
 }
 
--- include/lib-student/ecADC_student.h
+++ include/lib-student/ecADC_student.h
@@ -1,6 +1,8 @@
 #ifndef __MY_ADC_H
 #define __MY_ADC_H
 #include "stm32f411xe.h"
+#include "ecSTM32F411.h"
+
 
 // ADC trigmode
 #define SW 0
@@ -17,23 +19,45 @@
 
 #define _DEFAULT 0
 
-// ADC setting
-// Exercise- with Tutorial
-void ADC_init(GPIO_TypeDef *port, int pin, int trigmode);			// trigmode : SW , TRGO
+/////////////////////////////////////////////////////
+// ADC default setting
+/////////////////////////////////////////////////////
 
-// Exercise- with LAB
-void ADC_continue(int contmode); 													// contmode : CONT, SINGLE / Operate both ADC,JADC
-void ADC_TRGO(TIM_TypeDef* TIMx, int msec, int edge);
-void ADC_sequence(int length, int *seq); 
+// ADC init
+// Default:  one-channel mode, continuous conversion
+// Default: HW trigger - TIM3 counter, 1msec
+void ADC_init(PinName_t pinName);
+void JADC_init(PinName_t pinName);
 
-// Exercise- with Tutorial
+
+// Multi-Channel Scan Sequence 
+void ADC_sequence(PinName_t *seqCHn, int seqCHnums); 
+void JADC_sequence(PinName_t *seqCHn, int seqCHnums); 
+
 void ADC_start(void);
-uint32_t is_ADC_EOC(void);
-uint32_t ADC_read(void);
-uint32_t ADC_pinmap(GPIO_TypeDef *port, int pin);
+void JADC_start(void);
 
-// Exercise- with LAB
+// flag for ADC interrupt
+uint32_t is_ADC_EOC(void);
 uint32_t is_ADC_OVR(void);
 void clear_ADC_OVR(void);
+
+// read ADC value
+uint32_t ADC_read(void);
+
+
+/////////////////////////////////////////////////////
+// Advanced Setting
+/////////////////////////////////////////////////////
+// Conversion mode change: CONT, SINGLE / Operate both ADC,JADC
+void ADC_conversion(int convMode); 					
+void ADC_trigger(TIM_TypeDef* TIMx, int msec, int edge);
+
+// JADC setting
+void JADC_trigger(TIM_TypeDef* TIMx, int msec, int edge);
+
+
+// Private Function
+void ADC_pinmap(PinName_t pinName, uint32_t *chN);
 
 #endif
