81e180b6d85728f5cc456db551a4204248b2b7b5
Jorry BBong <147172379+Kwak-Jin@users.noreply.github.com>
2024-08-23 20:50:16 +0900

    Update and rename ecGPIO_student.c to ecGPIO2_student.c

--- include/lib-student/ecGPIO2_student.c
+++ include/lib-student/ecGPIO2_student.c
@@ -2,7 +2,7 @@
 @ Embedded Controller by Young-Keun Kim - Handong Global University
 Author           : SSS LAB
 Created          : 05-03-2021
-Modified         : 10-10-2023
+Modified         : 08-23-2024
 Language/ver     : C++ in Keil uVision
 
 Description      : Distributed to Students for LAB_GPIO
@@ -12,9 +12,13 @@ Description      : Distributed to Students for LAB_GPIO
 
 #include "stm32f4xx.h"
 #include "stm32f411xe.h"
-#include "ecGPIO_student.h"
+#include "ecGPIO2.h"
 
-void GPIO_init(GPIO_TypeDef *Port, int pin, int mode){     
+void GPIO_init(PinName_t pinName, uint32_t mode){     
+	GPIO_TypeDef * Port;
+	unsigned int pin;
+	ecPinmap(pinName, Port, &pin);
+	
 	// mode  : Input(0), Output(1), AlterFunc(2), Analog(3)   
 	if (Port == GPIOA)
 		RCC_GPIOA_enable();
@@ -27,39 +31,41 @@ void GPIO_init(GPIO_TypeDef *Port, int pin, int mode){
 	// You can also make a more general function of
 	// void RCC_GPIO_enable(GPIO_TypeDef *Port); 
 
-	GPIO_mode(Port, pin, mode);
-	
+	GPIO_mode(pinName, mode);
 }
 
 
 // GPIO Mode          : Input(00), Output(01), AlterFunc(10), Analog(11)
-void GPIO_mode(GPIO_TypeDef *Port, int pin, int mode){
+void GPIO_mode(PinName_t pinName, uint32_t mode){
+   GPIO_TypeDef * Port;
+   unsigned int pin;
+   ecPinmap(pinName,Port,&pin);
    Port->MODER &= ~(3UL<<(2*pin));     
    Port->MODER |= mode<<(2*pin);    
 }
 
 
 // GPIO Speed          : Low speed (00), Medium speed (01), Fast speed (10), High speed (11)
-void GPIO_ospeed(GPIO_TypeDef *Port, int pin, int speed){
+void GPIO_ospeed(PinName_t pinName, int speed){
 	//[TO-DO] YOUR CODE GOES HERE
 	//[TO-DO] YOUR CODE GOES HERE
 }
 
 // GPIO Output Type: Output push-pull (0, reset), Output open drain (1)
-void GPIO_otype(GPIO_TypeDef *Port, int pin, int type){
+void GPIO_otype(PinName_t pinName, int type){
    	//[TO-DO] YOUR CODE GOES HERE
 	//[TO-DO] YOUR CODE GOES HERE
 }
 
 // GPIO Push-Pull    : No pull-up, pull-down (00), Pull-up (01), Pull-down (10), Reserved (11)
-void GPIO_pupd(GPIO_TypeDef *Port, int pin, int pupd){
+void GPIO_pupd(PinName_t pinName, int pupd){
    	//[TO-DO] YOUR CODE GOES HERE
 	//[TO-DO] YOUR CODE GOES HERE
 }
 
-int GPIO_read(GPIO_TypeDef *Port, int pin){
+int GPIO_read(PinName_t pinName){
    	//[TO-DO] YOUR CODE GOES HERE
 	//[TO-DO] YOUR CODE GOES HERE
-	return 0;    	//[TO-DO] YOUR CODE GOES HERE	
+	return //[TO-DO] YOUR CODE GOES HERE	
 }
 
