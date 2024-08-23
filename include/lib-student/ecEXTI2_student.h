4860eafed2b804b13abe69cf8f733d40c16196ad
Jorry BBong <147172379+Kwak-Jin@users.noreply.github.com>
2024-08-23 20:41:39 +0900

    Update and rename ecEXTI_student.h to ecEXTI2_student.h

---
+++ include/lib-student/ecEXTI2_student.h
@@ -0,0 +1,24 @@
+#ifndef __EC_EXTI2_H
+#define __EC_EXTI2_H
+
+#include "stm32f411xe.h"
+
+#define FALL 0
+#define RISE 1
+#define BOTH 2
+
+#ifdef __cplusplus
+ extern "C" {
+#endif /* __cplusplus */
+
+void EXTI_init(PinName_t pinName, int trig_type,int priority);
+void EXTI_enable(PinName_t pinName);
+void EXTI_disable(PinName_t pinName);
+uint32_t is_pending_EXTI(PinName_t pinName);
+void clear_pending_EXTI(PinName_t pinName);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+	 
+#endif
