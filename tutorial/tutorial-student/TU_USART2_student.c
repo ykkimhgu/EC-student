0d3d80230fbe4546abb95f972e885982328156c4
Jorry BBong <147172379+Kwak-Jin@users.noreply.github.com>
2024-08-23 22:02:27 +0900

    Update TU_USART2_student.c

--- tutorial/tutorial-student/TU_USART2_student.c
+++ tutorial/tutorial-student/TU_USART2_student.c
@@ -1,16 +1,16 @@
 /**
 ******************************************************************************
 * @author  SSSLAB
-* @Mod		 2021-8-12 by YKKIM
+* @Mod     2024-8-23 by Jin Kwak
 * @brief   Embedded Controller:  Tutorial - USART communication
 *
 ******************************************************************************
 */
 
 #include "stm32f4xx.h"
-#include "ecGPIO.h"
-#include "ecRCC.h"
-#include "ecUART.h"
+#include "ecGPIO2.h"
+#include "ecRCC2.h"
+#include "ecUART2.h"
 
 #define END_CHAR 13
 
@@ -32,12 +32,12 @@ void setup(void)
 {
 	RCC_PLL_init();
 	USART_init(USART2, 9600);
-	USART_begin(USART1, GPIOA, 9, GPIOA, 10, 9600); 	// PA9 - RXD , PA10 - TXD
+	USART_setting(USART1, PA_9, PA_10, 9600); 	// PA9 - RXD , PA10 - TXD
 }
 
 void USART1_IRQHandler(){         //USART1 INT 
 	if(is_USART_RXNE(USART1)){
-		mcu2Data = USART_getc(USART1);
+		mcu2Data = fgetc(USART1);
 		printf("received: %c\r\n", mcu2Data);
 	}
 }
@@ -45,7 +45,7 @@ void USART1_IRQHandler(){         //USART1 INT
 
 void USART2_IRQHandler(){         //USART2 INT 
 	if(is_USART_RXNE(USART2)){
-		pcData = USART_getc(USART2);
+		pcData = fgetc(USART2);
 		USART_write(USART1, &pcData, 1);
 		printf("%c", pcData);
 		
