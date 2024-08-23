8bd295164b8725944ea41b458c2114caceb1a7ad
Jorry BBong <147172379+Kwak-Jin@users.noreply.github.com>
2024-08-23 20:53:29 +0900

    Update and rename ecGPIO_student.h to ecGPIO2_student.h

--- include/lib-student/ecGPIO2_student.h
+++ include/lib-student/ecGPIO2_student.h
@@ -2,7 +2,7 @@
 @ Embedded Controller by Young-Keun Kim - Handong Global University
 Author           : SSS LAB
 Created          : 05-03-2021
-Modified         : 09-20-2022
+Modified         : 08-23-2024
 Language/ver     : C++ in Keil uVision
 
 Description      : Distributed to Students for LAB_GPIO
@@ -10,10 +10,10 @@ Description      : Distributed to Students for LAB_GPIO
 
 
 #include "stm32f411xe.h"
-#include "ecRCC.h"
+#include "ecRCC2.h"
 
-#ifndef __ECGPIO_H
-#define __ECGPIO_H
+#ifndef __ECGPIO2_H
+#define __ECGPIO2_H
 
 #define INPUT  0x00
 #define OUTPUT 0x01
@@ -23,20 +23,20 @@ Description      : Distributed to Students for LAB_GPIO
 #define HIGH 1
 #define LOW  0
 
-#define LED_PIN 	5
-#define BUTTON_PIN 13
+#define LED_PIN    //Find LED Port&Pin and Fill the blank	
+#define BUTTON_PIN //Find BTN Port&Pin and Fill the blank
 
 #ifdef __cplusplus
  extern "C" {
 #endif /* __cplusplus */
 	 
-void GPIO_init(GPIO_TypeDef *Port, int pin, int mode);
-void GPIO_write(GPIO_TypeDef *Port, int pin, int Output);
-int  GPIO_read(GPIO_TypeDef *Port, int pin);
-void GPIO_mode(GPIO_TypeDef* Port, int pin, int mode);
-void GPIO_ospeed(GPIO_TypeDef* Port, int pin, int speed);
-void GPIO_otype(GPIO_TypeDef* Port, int pin, int type);
-void GPIO_pupd(GPIO_TypeDef* Port, int pin, int pupd);
+void GPIO_init(PinName_t pinName, uint32_t mode);     
+void GPIO_write(PinName_t pinName, int Output);
+int  GPIO_read(PinName_t pinName);
+void GPIO_mode(PinName_t pinName, uint32_t mode);
+void GPIO_ospeed(PinName_t pinName, int speed);
+void GPIO_otype(PinName_t pinName, int type);
+void GPIO_pupd(PinName_t pinName, int pupd);
 
 
  
