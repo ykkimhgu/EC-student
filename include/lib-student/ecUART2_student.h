ff6b61694811507cfaed2c2d1990bb5837738fcf
Jorry BBong <147172379+Kwak-Jin@users.noreply.github.com>
2024-08-23 21:56:54 +0900

    Update ecUART2_student.h

--- include/lib-student/ecUART2_student.h
+++ include/lib-student/ecUART2_student.h
@@ -59,7 +59,7 @@ void USART_init(USART_TypeDef* USARTx, uint32_t baud);
 void UART_baud(USART_TypeDef* USARTx, uint32_t baud);											
 uint32_t is_USART_RXNE(USART_TypeDef * USARTx);
 uint8_t USART_read(USART_TypeDef * USARTx);										
-void USART_setting(USART_TypeDef* USARTx, PinNmae_t pin_GPIO_TX, PinName_t pin_GPIO_TX, uint32_t baud); 
+void USART_setting(USART_TypeDef* USARTx, PinName_t pin_GPIO_TX, PinName_t pin_GPIO_RX, uint32_t baud); 
 void USART_delay(uint32_t us);  
 
 #endif // __EC_USART2_H
