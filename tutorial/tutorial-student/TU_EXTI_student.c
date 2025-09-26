484d0f6fcb8edd88ab2aa1c33640d25529770d6c
ykkimhgu <38373000+ykkimhgu@users.noreply.github.com>
2025-09-26 09:39:46 +0900

    Add files via upload

--- tutorial/tutorial-student/TU_EXTI_student.c
+++ tutorial/tutorial-student/TU_EXTI_student.c
@@ -1,7 +1,7 @@
 /**
 ******************************************************************************
 * @author  SSSLAB
-* @Mod		 2021-8-12 by YKKIM
+* @Mod		 2025-9-25 by YKKIM
 * @brief   Embedded Controller:  Tutorial ___
 *					 - _________________________________
 *
@@ -15,8 +15,8 @@
 #include "ecRCC2.h"
 #include "ecGPIO2.h"
 
-#define LED_PIN   PA_5
-#define BUTTON_PIN PC_13
+#define LED_PIN   PB_12
+#define BUTTON_PIN PA_4
 
 void setup(void);
 
@@ -38,8 +38,8 @@ void EXTI_init_tutorial(PinName_t pinName){
 	RCC->APB2ENR |= __________________
 
 	// Connect External Line to the GPIO
-	// Button: PC_13 -> EXTICR3(EXTI13)
-	SYSCFG->EXTICR[____] &= ~SYSCFG_EXTICR4_EXTI13;
+	// Button: PA_4 -> EXTICR2(EXTI4)
+	SYSCFG->EXTICR[____] &= ~SYSCFG_EXTICR2_EXTI4;
 	SYSCFG->EXTICR[____] |= ______________________;
 
 	// Falling trigger enable (Button: pull-up)
@@ -49,15 +49,15 @@ void EXTI_init_tutorial(PinName_t pinName){
 	EXTI->IMR |= 1UL << ___________;
 
 	// Interrupt IRQn, Priority
-	NVIC_SetPriority(EXTI15_10_IRQn, 0);  		// Set EXTI priority as 0	
-	NVIC_EnableIRQ(EXTI15_10_IRQn); 			// Enable EXTI 
+	NVIC_SetPriority(EXTI4_IRQn, 0);  		// Set EXTI priority as 0	
+	NVIC_EnableIRQ(EXTI4_IRQn); 			// Enable EXTI 
 	
 }
 
-void EXTI15_10_IRQHandler(void) {
-	if ((EXTI->PR & EXTI_PR_PR13) == _________) {
+void EXTI4_IRQHandler(void) {
+	if ((EXTI->PR & EXTI_PR_PR4) == _________) {
 		LED_toggle();
-		EXTI->PR |= EXTI_PR_PR13; // cleared by writing '1'
+		EXTI->PR |= EXTI_PR_PR4; // cleared by writing '1'
 	}
 }
 
@@ -66,11 +66,10 @@ void EXTI15_10_IRQHandler(void) {
 void setup(void)
 {
 	RCC_PLL_init();                         // System Clock = 84MHz
-	// Initialize GPIOA_5 for Output
-	GPIO_init(LED_PIN, OUTPUT);    // calls RCC_GPIOA_enable()	
-	// Initialize GPIOC_13 for Input Button
-	GPIO_init(BUTTON_PIN, INPUT);  // calls RCC_GPIOC_enable()
-	EXTI_init_tutorial(PC_13);
+	// Initialize GPIOB_12 for Output
+	GPIO_init(LED_PIN, OUTPUT);    // LED for EVAL board	
+	// Initialize GPIOA_4 for Input Button
+	GPIO_init(BUTTON_PIN, INPUT);  // OUTPUT for EVAL borad
+	EXTI_init_tutorial(PA_4);
 		
 }
-
--- tutorial/tutorial-student/TU_SysTick_student.c
+++ tutorial/tutorial-student/TU_SysTick_student.c
@@ -1,12 +1,19 @@
+/**
+******************************************************************************
+* @author  IILAB
+* @Mod	   2025-9-25 by YKKIM
+* @brief   Embedded Controller:  Tutorial ____________________________________
+*
+******************************************************************************
+*/
+
+
 #include "stm32f411xe.h"
 #include "ecRCC2.h"
 #include "ecGPIO2.h"
-
-#define MCU_CLK_PLL 84000000
-#define MCU_CLK_HSI 16000000
+#include "ecSysTick2.h"
 
 volatile uint32_t msTicks = 0;
-volatile uint32_t curTicks;
 
 void setup(void);
 
@@ -15,52 +22,22 @@ int main(void) {
 // System CLOCK, GPIO Initialiization ----------------------------------------
 	setup();
 
-// SysTick Initialiization ------------------------------------------------------				
-	//  SysTick Control and Status Register
-	SysTick->CTRL = 0;				// Disable SysTick IRQ and SysTick Counter
-
-	// Select processor clock
-	// 1 = processor clock;  0 = external clock
-	SysTick->CTRL |= __________
-
-	// uint32_t MCU_CLK=EC_SYSTEM_CLK
-	// SysTick Reload Value Register
-	SysTick->LOAD =__________ 				// 1ms
-
-	// Clear SysTick Current Value 
-	SysTick->VAL = 0;
-
-	// Enables SysTick exception request
-	// 1 = counting down to zero asserts the SysTick exception request
-	SysTick->CTRL |= __________
-		
-	// Enable SysTick IRQ and SysTick Timer
-	SysTick->CTRL |= __________
-		
-	NVIC_SetPriority(SysTick_IRQn, 16);		// Set Priority to 1
-	NVIC_EnableIRQ(SysTick_IRQn);			// Enable interrupt in NVIC
-
-		
 // While loop ------------------------------------------------------				
-	msTick = 0;
 
 	while(1){
-		curTicks = msTicks;
-		while ((msTicks - curTicks) < 1000);	
-		msTicks = 0;
-		LED_toggle();
+        GPIO_write(PB_12, HIGH);        
+        delay_ms (1000);
+        GPIO_write(PB_12, LOW);        
+        delay_ms (1000);        
 	}
 }
 
 
-void SysTick_Handler(void){
-	msTicks++;
-}
 
 void setup(void)
 {
 	RCC_PLL_init();              // System Clock = 84MHz	
-	GPIO_init(PA_5, OUTPUT);     // calls RCC_GPIOA_enable()	
+	//GPIO_init(PA_5, OUTPUT);     // LED for Nucleo
+   	GPIO_init(PB_12, OUTPUT);  // LED for Eval Board	JKIT
+    SysTick_init(); 
 }
-
-
